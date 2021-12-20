3b8d15d9e5354fabbe20099c2aab7451a9eeccfc
S-B-Iqbal <buland.shahrukh@gmail.com>
2021-12-20 10:14:24 +0100

    Update data_preprocessing.py

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -14,29 +14,22 @@ from tqdm import tqdm
 import numpy as np
 import pandas as pd
 
-reuters = load_dataset("reuters21578","ModApte") # ModApte b'coz of "A re-examination of text categorization methods" paper
-
-train = reuters['train'] # Same as paper
-test = reuters['test'] # Same as paper
-
-train.set_format(type = "pandas")
-test.set_format(type = "pandas")
-
-df_train = train[:]
-df_test = test[:]
-
-df_train = df_train[~df_train.topics.str.len().eq(0)] # Drop Empty Topics
-df_test = df_test[~df_test.topics.str.len().eq(0)] # Drop Empty Topics
-
-cols =df_train.columns
-
-df_train = df_train.drop([col for col in cols if col not in ['text', 'topics']], axis=1)
-df_test = df_test.drop([col for col in cols if col not in ['text', 'topics']], axis=1)
-
-X_train = df_train['text']
-X_test = df_test['text']
-
-mlb = MultiLabelBinarizer() @ Multi-Lable Problem
-
-y_train = mlb.fit_transform(df_train.topics)
-y_test = mlb.transform(df_test.topics)
+def get_data():
+    reuters = load_dataset("reuters21578","ModApte") # ModApte b'coz of "A re-examination of text categorization methods" paper
+    train = reuters['train'] # Same as paper
+    test = reuters['test'] # Same as paper
+    train.set_format(type = "pandas")
+    test.set_format(type = "pandas")   
+    df_train = train[:]
+    df_test = test[:]
+    df_train = df_train[~df_train.topics.str.len().eq(0)] # Drop Empty Topics
+    df_test = df_test[~df_test.topics.str.len().eq(0)] # Drop Empty Topics
+    cols =df_train.columns
+    df_train = df_train.drop([col for col in cols if col not in ['text', 'topics']], axis=1)
+    df_test = df_test.drop([col for col in cols if col not in ['text', 'topics']], axis=1)
+    X_train = df_train['text']
+    X_test = df_test['text']
+    mlb = MultiLabelBinarizer()
+    y_train = mlb.fit_transform(df_train.topics)
+    y_test = mlb.transform(df_test.topics)
+    return X_train,X_test,y_train,y_test
